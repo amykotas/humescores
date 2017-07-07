c576092f62f3b15468da66a828e1d415fd0803bd
Amy Kotas <aekotas@gmail.com>
2017-07-07 06:31:40 -0400

    Forked up everything, reloaded from class exercise files. 💩

--- wp-content/themes/humescores/header.php
+++ wp-content/themes/humescores/header.php
@@ -30,13 +30,12 @@
 		</a>
 	</figure><!-- .header-image -->
 	<?php endif; // End header image check. ?>
-
+	
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-
+			
 			<?php the_custom_logo(); ?>
 			<div class="site-branding__text">
-
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -50,7 +49,7 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
-			</div> <!-- .site-branding__text -->
+			</div><!-- .site-branding__text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
